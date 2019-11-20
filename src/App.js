9934da9f5bf35e5929d18b6de016c8f3ec249021
Heber <hebergonza@gmail.com>
2019-11-19 21:53:29 -0500

    added alt to images

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from "react";
 import "./App.css";
 import logo from "./assets/nasa_logo.png";
-import PictureCard from "../src/components/PictureCard"
 import PictureData from "../src/components/PictureData"
 
 function App() {
@@ -9,7 +8,7 @@ function App() {
     <div className="App">
       <div className="container">
         <h1>Nasa Photo of the Day</h1>
-        <img className="logo" src={logo}/>
+        <img className="logo" src={logo} alt="NASA Logo"/>
         <PictureData />
       </div>
     </div>
--- src/components/PictureCard.js
+++ src/components/PictureCard.js
@@ -4,7 +4,7 @@ const PictureCard = (props) => {
   return (
     <div>
       <h2>Date: {props.picture.date}</h2>
-      <img src={props.picture.url}></img>
+      <img src={props.picture.url} alt={props.picture.title}></img>
       <h2>Picture Title: {props.picture.title}</h2>
       <h2>Author: {props.picture.copyright}</h2>
       <h2>Explanation: {props.picture.explanation}</h2>
